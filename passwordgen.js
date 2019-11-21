e193bfb62f7cb171ca071baabcaef36dff262bf3
Jyles Coad-Ward <26735262+jylescoad-ward@users.noreply.github.com>
2019-11-21 10:28:54 +0800

    build18
    first upload to github
    added process.argv so the user can give the password length if they want to

---
+++ passwordgen.js
@@ -0,0 +1,13 @@
+if (process.argv.length <= 2){
+	console.log("No Length Specifiyed, Exiting Process.");
+	console.log(" (you gotta do, node passwordgen.js 128 for 128 characters)");
+} else {
+
+var length = process.argv[2];
+var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-=\;',./`!@#$%^&*()_+|:?><";
+var retVal = "";
+for (var i = 0, n = charset.length; i < length; ++i) {
+	retVal += charset.charAt(Math.floor(Math.random() * n));
+}
+console.log(retVal);
+}
